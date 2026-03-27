357d0d17ac8f96256bf5578e8b4a3071d297e431
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-03-27 08:31:55 -0400

    feat: add expor_pod

---
+++ expor_pod.sh
@@ -0,0 +1,18 @@
+#expor pod - service
+#nao rodar esse script, ele tem apenas a funcao de mostrar os comandos
+# Fonte: cursos do Girus da LinuxTips/descomplicando k8s
+
+#visite o arquivo dry-run.sh e gere o pod como o comando manda
+
+#apague o pod
+kubectl delete -f pod-template.yaml
+
+#modificando o pod-template
+kubectl run meu-nginx --image nginx --port 80 --dry-run=client -o yaml > pod-template.yaml
+kubectl create -f pod-template.yaml
+
+#cria um service
+kubectl expose pod meu-nginx
+
+#teste basico
+kubectl port-forward svc/meu-nginx 8080:80
