92cafeba1f8a6e58edcc67e7bd5d42983588eb0a
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-04-08 14:34:42 -0400

    chore: template do criando_replicaset

---
+++ day4/ReplicaSet/criando_replicaset.sh
@@ -0,0 +1,6 @@
+#basico sobre criar replicaset
+#nao rodar esse script, ele tem apenas a funcao de mostrar os comandos
+# Fonte: cursos do Girus da LinuxTips/descomplicando k8s
+
+#de preferencia nao crie um replciaset sem um deployment
+
